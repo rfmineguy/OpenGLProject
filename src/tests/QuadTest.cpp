e5f96b3c642a9a1a38b4b42b8f7b4f64b248ed8f
Riley Fischer <rfmineguy@gmail.com>
2021-02-13 20:09:25 -0800

    committing a change to 'experiments'
    Finished QuadTest :
    QuadTest contains all the raw OpenGL code to setup and render VAOs, VBOs, and IBOs.

--- src/tests/QuadTest.cpp
+++ src/tests/QuadTest.cpp
@@ -5,96 +5,127 @@
 #include "QuadTest.h"
 
 test::QuadTest::QuadTest() {
-    /** VERTEX SHADER **/
-    const char *vertexShaderSource = "#version 330 core\n"
-                                     "layout (location = 0) in vec3 aPos;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                     "}\0";
-    m_VertShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(m_VertShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(m_VertShader);
-    // check for shader compile errors
     int success;
     char infoLog[512];
-    glGetShaderiv(m_VertShader, GL_COMPILE_STATUS, &success);
-    if (!success)
+    /** VERTEX SHADER **/
     {
-        glGetShaderInfoLog(m_VertShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        const char *vertexShaderSource = "#version 450 core\n"
+                                         "layout (location = 0) in vec3 aPos;\n"
+                                         "void main()\n"
+                                         "{\n"
+                                         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                         "}\0";
+        m_VertShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(m_VertShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(m_VertShader);
+        // check for shader compile errors
+        glGetShaderiv(m_VertShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(m_VertShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        } else {
+            std::cout << "INFO::SHADER::VERTEX::COMPILATION_SUCCESS\n" << std::endl;
+        }
     }
     /** VERTEX SHADER **/
 
-
     /** FRAG SHADER **/
-    const char *fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                       "}\n\0";
-
-    m_FragShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(m_FragShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(m_FragShader);
-    // check for shader compile errors
-    glGetShaderiv(m_FragShader, GL_COMPILE_STATUS, &success);
-    if (!success)
     {
-        glGetShaderInfoLog(m_FragShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+        const char *fragmentShaderSource = "#version 450 core\n"
+                                           "out vec4 FragColor;\n"
+                                           "void main()\n"
+                                           "{\n"
+                                           "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                           "}\n\0";
+
+        m_FragShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(m_FragShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(m_FragShader);
+        // check for shader compile errors
+        glGetShaderiv(m_FragShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(m_FragShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        } else {
+            std::cout << "INFO::SHADER::FRAGMENT::COMPILATION_SUCCESS\n" << std::endl;
+        }
     }
     /** FRAG SHADER **/
 
 
     /** SHADER PROGRAM **/
-    m_Program = glCreateProgram();
-    glAttachShader(m_Program, m_VertShader);
-    glAttachShader(m_Program, m_FragShader);
-    glLinkProgram(m_Program);
-    // check for linking errors
-    glGetProgramiv(m_Program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(m_Program, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    {
+        m_Program = glCreateProgram();
+        std::cout << m_Program << std::endl;
+        glAttachShader(m_Program, m_VertShader);
+        glAttachShader(m_Program, m_FragShader);
+        glLinkProgram(m_Program);
+        glValidateProgram(m_Program);
+        // check for linking errors
+        glGetProgramiv(m_Program, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(m_Program, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        } else {
+            std::cout << "INFO::SHADER::PROGRAM::LINKING_SUCCESS\n" << infoLog << std::endl;
+        }
+        glDeleteShader(m_VertShader);
+        glDeleteShader(m_FragShader);
+        glUseProgram(m_Program);
     }
-    glDeleteShader(m_VertShader);
-    glDeleteShader(m_FragShader);
     /** SHADER PROGRAM **/
 
     /** BUFFERS **/
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, // right
-            0.0f,  0.5f, 0.0f  // top
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
     };
 
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 3, 1,  // first Triangle
+            1, 3, 2   // second Triangle
+    };
     glGenVertexArrays(1, &m_VAO);
-    glGenBuffers(1, &m_VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(m_VAO);
 
+    glGenBuffers(1, &m_VBO);
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 
-    printf("push this to experiments");
+    glGenBuffers(1, &m_IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(indices), indices);
+
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+    //glBindVertexArray(0);
+
     /** BUFFERS **/
 }
 
 test::QuadTest::~QuadTest() {
+    printf("Destructed");
     glDeleteVertexArrays(1, &m_VAO);
     glDeleteBuffers(1, &m_VBO);
+    glDeleteBuffers(1, &m_IBO);
     glDeleteProgram(m_Program);
 }
 
@@ -103,9 +134,13 @@ void test::QuadTest::OnUpdate(double dt) {
 }
 
 void test::QuadTest::OnRender() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glUseProgram(m_Program);
-    glBindVertexArray(m_VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(m_VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 }
 
 void test::QuadTest::OnResize(int width, int height) {
